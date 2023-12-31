3d99403dfa39694de448a7dc679ea97a6531db33
Sean Moriarty <sean@sean-m.net>
2023-12-31 11:14:32 -0700

    CTRL+q to quit implemented

--- kilo.c
+++ kilo.c
@@ -7,6 +7,10 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** defines ***/
+
+#define CRTL_KEY(k) ((k) & 0x1f)
+
 /*** data ***/
 
 struct termios orig_termios;
@@ -52,7 +56,7 @@ int main() {
         } else {
             printf("%d ('%c')\r\n", c, c);
         }
-        if (c == 'q') break;
+        if (c == CRTL_KEY('q')) break;
     }    
 
     return 0;
